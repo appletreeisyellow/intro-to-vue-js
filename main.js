1fd150f4ee0b834cd48b878ef258f0b66395ac97
Vivo Ye <yccvivo@gmail.com>
2020-06-04 15:26:16 -0700

    add class and style binding

--- main.js
+++ main.js
@@ -5,6 +5,35 @@ var app = new Vue({
     image: './assets/vmSocks-green.jpg',
     link: "https://www.chunchunye.com",
     inventory: 1,
+    inStock: false,
     onSale: true,
+    variants: [
+      {
+        variantId: 2233,
+        variantColor: "green",
+        variantImage: "./assets/vmSocks-green.jpg"
+      },
+      {
+        variantId: 2234,
+        variantColor: "blue",
+        variantImage: "./assets/vmSocks-blue.jpg"
+      }
+    ],
+    sizes: [1, 2, 3, 4, 5],
+    cart: 10
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    removeFromCart() {
+      if (this.cart < 1){
+        return
+      }
+      this.cart -= 1
+    },
+    updateProduct(image) {
+      this.image = image
+    }
   }
 })
